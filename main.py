d5daa3040056c357526f90be7ca64ce30ee247bc
jemre <moore.doubleo@gmail.com>
2020-02-23 16:46:53 -0600

    feat: add smoothing to sales transactions
    * chore: update dependencies
    
    * feat: add smoothing to transactions

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from data_access_object import DataAccessObject
 def main():
     data_access_object = DataAccessObject()
 
+    print('loading data...')
     data_access_object.load_data()
-    data_access_object.calculate_social_media_score()
 
-    data_access_object.data['social_media_activity_score'].to_csv('social_media_activity_scores.csv', index=False)
+    print('calculating social media activity score...')
+    data_access_object.calculate_social_media_score()
+    data_access_object.data['social_media_activity_score'].to_csv('data/social_media_activity_scores.csv', index=False)
+
+    print('smoothing sales...')
+    data_access_object.calculate_sales_smoothing()
+    data_access_object.data['sales_with_smoothing'].to_csv('data/sales_with_smoothing.csv', index=False)
+
 
 if __name__ == '__main__':
     main()
