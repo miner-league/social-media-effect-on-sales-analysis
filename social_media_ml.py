1c416a0a4bd5935adee2a26c034860a7d88f3c4b
Purna Kuntla <kpcreddy@hotmail.com>
2020-03-15 17:41:38 -0500

    Multiple linear regression heatmap

--- social_media_ml.py
+++ social_media_ml.py
@@ -1,4 +1,5 @@
-
+import csv
+import datetime
 import pandas as pd
 import statsmodels.api as sm
 from sklearn.impute import SimpleImputer
@@ -112,7 +113,6 @@ def replace_missing_value(df, number_features):
     res_def = pd.DataFrame(X, columns=df_num.columns)
     return res_def
 
-
 def process_data():
     twitter_data = get_twitter_social_media_data()
     twitter_columns = ['Date', 'Twitter Profile', 'Twitter_Followers', 'Twitter_Net Follower Growth',
@@ -200,7 +200,7 @@ def process_data():
     # print('Coefficients: \n', regressor.coef_)
     X = sm.add_constant(X)
     model = sm.OLS(Y, X).fit()
-    predictions = model.predict(X)
+    # predictions = model.predict(X)
     # print_model = model.summary().tables[1]
     # print(model.rsquared)
     # print(print_model)
@@ -217,13 +217,164 @@ def process_data():
     regressor2.fit(X2, Y)
     X2 = sm.add_constant(X2)
     model2 = sm.OLS(Y, X2).fit()
-    predictions2 = model2.predict(X2)
+    # predictions2 = model2.predict(X2)
     print_model2 = model2.summary().tables[1]
     print(model2.rsquared_adj)
     print(print_model2)
 
+def get_combined_social_data():
+    twitter_data = get_twitter_social_media_data()
+    twitter_columns = ['Date', 'Twitter Profile', 'Twitter_Followers', 'Twitter_Net Follower Growth',
+       'Twitter_Following', 'Twitter_Net Following Growth', 'Twitter_Published Posts', 'Twitter_Impressions',
+       'Twitter_Video Views', 'Twitter_Engagements', 'Twitter_Likes', 'Twitter_@Replies', 'Twitter_Retweets',
+       'Twitter_Post Link Clicks', 'Twitter_Other Post Clicks', 'Twitter_Other Engagements',
+       'Twitter_Engagement Rate (per Impression)']
+    twitter_data.columns = twitter_columns
+    # print(twitter_data.columns)
+    # print(twitter_columns)
+    facebook_data = get_facebook_social_media_data()
+    facebook_columns = ['Date', 'Facebook Page', 'Facebook_Total Fans', 'Facebook_Net Page Likes', 'Facebook_Page Likes',
+       'Facebook_Organic Page Likes', 'Facebook_Paid Page Likes', 'Facebook_Page Unlikes',
+       'Facebook_Published Posts', 'Facebook_Impressions', 'Facebook_Organic Impressions',
+       'Facebook_Paid Impressions', 'Facebook_Reach', 'Facebook_Organic Reach', 'Facebook_Paid Reach',
+       'Facebook_Engagements', 'Facebook_Reactions', 'Facebook_Likes', 'Facebook_Love Reactions', 'Facebook_Haha Reactions',
+       'Facebook_Wow Reactions', 'Facebook_Sad Reactions', 'Facebook_Angry Reactions', 'Facebook_Comments',
+       'Facebook_Shares', 'Facebook_Post Link Clicks', 'Facebook_Other Post Clicks', 'Facebook_Page Actions',
+       'Facebook_Engagement Rate (per Impression)', 'Facebook_Negative Feedback', 'Facebook_Video Views',
+       'Facebook_Full Video Views', 'Facebook_Partial Video Views', 'Facebook_Organic Video Views',
+       'Facebook_Organic Full Video Views', 'Facebook_Organic Partial Video Views',
+       'Facebook_Paid Video Views', 'Facebook_Paid Full Video Views', 'Facebook_Paid Partial Video Views',
+       'Facebook_Click to Play Video Views', 'Facebook_Autoplay Video Views']
+    facebook_data.columns = facebook_columns
+    # print(facebook_data.columns)
+    # print(facebook_columns)
+    instagram_data = get_instagram_social_media_data()
+    instagram_columns = ['Date', 'Instagram Profile', 'Instagram_Followers', 'Instagram_Net Follower Growth',
+       'Instagram_Followers Gained', 'Instagram_Followers Lost', 'Instagram_Following',
+       'Instagram_Net Following Growth', 'Instagram_Published Posts & Stories', 'Instagram_Impressions',
+       'Instagram_Reach', 'Instagram_Engagements', 'Instagram_Likes', 'Instagram_Comments', 'Instagram_Saves', 'Instagram_Story Replies','Instagram_Profile Actions', 'Instagram_Engagement Rate (per Impression)']
+    instagram_data.columns = instagram_columns
+    # print(instagram_data.columns)
+    # print(instagram_columns)
+
+    tf = pd.merge(twitter_data, facebook_data, on='Date')
+    tfi = pd.merge(tf, instagram_data, on='Date')
+    return tfi
+
+def generate_heatmap():
+    tfi = get_combined_social_data()
+    x_columns = ['Twitter_Followers',
+                 'Twitter_Net Follower Growth', 'Twitter_Following',
+                 'Twitter_Net Following Growth', 'Twitter_Published Posts',
+                 'Twitter_Impressions', 'Twitter_Video Views', 'Twitter_Engagements',
+                 'Twitter_Likes', 'Twitter_@Replies', 'Twitter_Retweets',
+                 'Twitter_Post Link Clicks', 'Twitter_Other Post Clicks',
+                 'Twitter_Other Engagements', 'Twitter_Engagement Rate (per Impression)',
+                 'Facebook_Total Fans', 'Facebook_Net Page Likes',
+                 'Facebook_Page Likes', 'Facebook_Organic Page Likes',
+                 'Facebook_Paid Page Likes', 'Facebook_Page Unlikes',
+                 'Facebook_Published Posts', 'Facebook_Impressions',
+                 'Facebook_Organic Impressions', 'Facebook_Paid Impressions',
+                 'Facebook_Reach', 'Facebook_Organic Reach', 'Facebook_Paid Reach',
+                 'Facebook_Engagements', 'Facebook_Reactions', 'Facebook_Likes',
+                 'Facebook_Love Reactions', 'Facebook_Haha Reactions',
+                 'Facebook_Wow Reactions', 'Facebook_Sad Reactions',
+                 'Facebook_Angry Reactions', 'Facebook_Comments', 'Facebook_Shares',
+                 'Facebook_Post Link Clicks', 'Facebook_Other Post Clicks',
+                 'Facebook_Page Actions', 'Facebook_Engagement Rate (per Impression)',
+                 'Facebook_Negative Feedback', 'Facebook_Video Views',
+                 'Facebook_Full Video Views', 'Facebook_Partial Video Views',
+                 'Facebook_Organic Video Views', 'Facebook_Organic Full Video Views',
+                 'Facebook_Organic Partial Video Views', 'Facebook_Paid Video Views',
+                 'Facebook_Paid Full Video Views', 'Facebook_Paid Partial Video Views',
+                 'Facebook_Click to Play Video Views', 'Facebook_Autoplay Video Views',
+                 'Instagram_Followers',
+                 'Instagram_Net Follower Growth', 'Instagram_Followers Gained',
+                 'Instagram_Followers Lost', 'Instagram_Following',
+                 'Instagram_Net Following Growth', 'Instagram_Published Posts & Stories',
+                 'Instagram_Impressions', 'Instagram_Reach', 'Instagram_Engagements',
+                 'Instagram_Likes', 'Instagram_Comments', 'Instagram_Saves',
+                 'Instagram_Story Replies', 'Instagram_Profile Actions',
+                 'Instagram_Engagement Rate (per Impression)']
+    df_smoothed_sales = pd.read_csv('data/sales_with_smoothing.csv')
+    df_smoothed_sales = df_smoothed_sales[['Date', 'StoreId', 'SmoothedTransactionCount']]
+    df_smoothed_sales['Date'] = pd.to_datetime(df_smoothed_sales.Date, yearfirst=True)
+    i = 1
+    header = ['Stores', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+              27, 28, 29, 30]
+    matrixtable = [header]
+    rsquared_adj_row = ['All']
+    while i <= 30:
+        df_smoothed_sales['Date'] += datetime.timedelta(days=i)
+        df_smoothed_sales_group = df_smoothed_sales.groupby(['Date'])[["SmoothedTransactionCount"]].sum()
+        tfi_smooth_trans = pd.merge(tfi, df_smoothed_sales_group, on='Date')
+        X = tfi_smooth_trans[x_columns]
+        X = replace_missing_value(X, X.columns)
+        Y = tfi_smooth_trans['SmoothedTransactionCount']
+        regressor = LinearRegression()
+        regressor.fit(X, Y)
+        X = sm.add_constant(X)
+        model = sm.OLS(Y, X).fit()
+        pred_p_values = model.pvalues[model.pvalues < 0.05]
+        pred_x_columns = pred_p_values.keys().tolist()
+        if 'const' in pred_x_columns:
+            pred_x_columns.remove('const')
+
+        # Model with predominant features
+        rsquared_adj = 0
+        if len(pred_x_columns) >= 1:
+            X2 = tfi_smooth_trans[pred_x_columns]
+            X2 = replace_missing_value(X2, X2.columns)
+            regressor2 = LinearRegression()
+            regressor2.fit(X2, Y)
+            X2 = sm.add_constant(X2)
+            model2 = sm.OLS(Y, X2).fit()
+            rsquared_adj = model2.rsquared_adj
+        rsquared_adj_row.append(rsquared_adj)
+        i += 1
+    matrixtable.append(rsquared_adj_row)
+    # Get the unique values of 'B' column
+    stores = df_smoothed_sales.StoreId.unique()
+    # Using for loop
+    for storeid in stores:
+        df_filtered_sales = df_smoothed_sales[df_smoothed_sales['StoreId'] == storeid]
+        i = 1
+        rsquared_adj_row = [storeid]
+        while i <= 30:
+            df_filtered_sales['Date'] += datetime.timedelta(days=i)
+            df_smoothed_sales_group = df_filtered_sales.groupby(['Date'])[["SmoothedTransactionCount"]].sum()
+            tfi_smooth_trans = pd.merge(tfi, df_smoothed_sales_group, on='Date')
+            X = tfi_smooth_trans[x_columns]
+            X = replace_missing_value(X, X.columns)
+            Y = tfi_smooth_trans['SmoothedTransactionCount']
+            regressor = LinearRegression()
+            regressor.fit(X, Y)
+            X = sm.add_constant(X)
+            model = sm.OLS(Y, X).fit()
+            pred_p_values = model.pvalues[model.pvalues < 0.05]
+            pred_x_columns = pred_p_values.keys().tolist()
+            if 'const' in pred_x_columns:
+                pred_x_columns.remove('const')
+
+            # Model with predominant features
+            rsquared_adj = 0
+            if len(pred_x_columns) >= 1:
+                X2 = tfi_smooth_trans[pred_x_columns]
+                X2 = replace_missing_value(X2, X2.columns)
+                regressor2 = LinearRegression()
+                regressor2.fit(X2, Y)
+                X2 = sm.add_constant(X2)
+                model2 = sm.OLS(Y, X2).fit()
+                rsquared_adj = model2.rsquared_adj
+            rsquared_adj_row.append(rsquared_adj)
+            i += 1
+        matrixtable.append(rsquared_adj_row)
+    with open("data/multiple_lr_heatmap.csv", "w+") as multiple_lr_heatmap:
+        csvWriter = csv.writer(multiple_lr_heatmap, delimiter=',')
+        csvWriter.writerows(matrixtable)
 if __name__ == '__main__':
-    process_data()
+    generate_heatmap()
+    # process_data()
 
 
 
