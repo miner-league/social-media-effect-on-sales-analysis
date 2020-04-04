fc1783077366af89e7d2f525d18f2efd8ef6b0ad
Jerry Jacob <jerjel@gmail.com>
2020-04-04 13:33:38 -0500

    PCA analysis

--- social_media_ml.py
+++ social_media_ml.py
@@ -8,6 +8,7 @@ from utilities import remove_commas, remove_percent
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def get_twitter_social_media_data():
@@ -421,7 +422,7 @@ def generate_mlfit_loyalty():
         csvWriter.writerows(matrixtable)
 
 
-x_columns1 = ['Twitter_Followers',
+x_columns = ['Twitter_Followers',
               'Twitter_Net Follower Growth', 'Twitter_Following',
               'Twitter_Net Following Growth', 'Twitter_Published Posts',
               'Twitter_Impressions', 'Twitter_Video Views', 'Twitter_Engagements',
@@ -712,8 +713,133 @@ def addMatrixTableRow(x_columns, matrixtable, rows, df, Y):
     return matrixtable
 
 
+def pca_analysis():
+    tfi = get_combined_social_data()
+    tfi['month'] = tfi['Date'].dt.month
+    tfi['year'] = tfi['Date'].dt.year
+    tfi = tfi.drop(columns=['Date'])
+    tfi_aggregated = tfi.groupby(['year', 'month'], as_index=False).sum()
+
+    ##### Store based ML analysis #####
+    sales_df_store = get_combined_sales_data('Store')
+    all_store_sales = pd.merge(
+        tfi_aggregated,
+        sales_df_store,
+        how='inner',
+        on=['year', 'month']
+    )
+    all_store_sales = all_store_sales.groupby(['year', 'month'], as_index=False).sum()
+    all_store_sales = all_store_sales.dropna()
+    X = all_store_sales[x_columns]
+    X = replace_missing_value(X, X.columns)
+    Y = all_store_sales['SmoothedTransactionCount']
+    pca_process_xy(X, Y)
+
+
+def pca_process_xy(x, y):
+
+    # Splitting the X and Y into the
+    # Training set and Testing set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+    # performing preprocessing part
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    # Applying PCA function on training
+    # and testing set of X component
+    from sklearn.decomposition import PCA
+
+    pca = PCA(n_components = 2)
+
+    X_train = pca.fit_transform(X_train)
+    X_test = pca.transform(X_test)
+
+    explained_variance = pca.explained_variance_ratio_
+
+    # Fitting Logistic Regression To the training set
+    from sklearn.linear_model import LogisticRegression
+
+    classifier = LogisticRegression(random_state=0)
+    classifier.fit(X_train, y_train)
+
+    # Predicting the test set result using
+    # predict function under LogisticRegression
+    y_pred = classifier.predict(X_test)
+
+    # making confusion matrix between
+    # test set of Y and predicted value.
+    from sklearn.metrics import confusion_matrix
+
+    cm = confusion_matrix(y_test, y_pred)
+
+    # Predicting the training set
+    # result through scatter plot
+    from matplotlib.colors import ListedColormap
+
+    X_set, y_set = X_train, y_train
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+                        stop = X_set[:, 0].max() + 1, step = 0.01),
+                        np.arange(start = X_set[:, 1].min() - 1,
+                        stop = X_set[:, 1].max() + 1, step = 0.01))
+
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),
+                X2.ravel()]).T).reshape(X1.shape), alpha = 0.75,
+                cmap = ListedColormap(('yellow', 'white', 'aquamarine')))
+
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+
+    plt.title('Logistic Regression (Training set)')
+    plt.xlabel('PC1') # for Xlabel
+    plt.ylabel('PC2') # for Ylabel
+    plt.legend() # to show legend
+
+    # show scatter plot
+    plt.show()
+
+    # Visualising the Test set results through scatter plot
+    from matplotlib.colors import ListedColormap
+
+    X_set, y_set = X_test, y_test
+
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+                        stop = X_set[:, 0].max() + 1, step = 0.01),
+                        np.arange(start = X_set[:, 1].min() - 1,
+                        stop = X_set[:, 1].max() + 1, step = 0.01))
+
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),
+                X2.ravel()]).T).reshape(X1.shape), alpha = 0.75,
+                cmap = ListedColormap(('yellow', 'white', 'aquamarine')))
+
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+
+    # title for scatter plot
+    plt.title('Logistic Regression (Test set)')
+    plt.xlabel('PC1') # for Xlabel
+    plt.ylabel('PC2') # for Ylabel
+    plt.legend()
+
+    # show scatter plot
+    plt.show()
+
+
 if __name__ == '__main__':
     # generate_heatmap_transactions()
     # generate_mlfit_loyalty()
-    generate_mlfit_loyalty1()
-    generate_mlfit_sales()
+    # generate_mlfit_loyalty1()
+    # generate_mlfit_sales()
+    pca_analysis()
